0294cb2011c0d5c567ff28b31b08235eccbf0201
Peter Weisdepp <weisdepp@seattleu.edu>
2018-04-28 18:24:03 -0700

    Add some more unit tests

--- PA2/Driver.cs
+++ PA2/Driver.cs
@@ -1,20 +1,19 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PA2
 {
     class Driver
     {
-
         public void Run()
         {
+            Console.WriteLine("");
             EncryptWord ew = new EncryptWord();
             ew.setEncryptedWord("Tester");
             Console.WriteLine(ew.getEncryptedWord());
-            Console.ReadLine();
+
+
+            Console.WriteLine("Press any key to close window...");
+            Console.ReadLine(); // use so console window doesn't disappear immediately
         }
     }
 }
